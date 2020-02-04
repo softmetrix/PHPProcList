c5a90377e7a072af047e3eb1720932a209052ccf
Marko Medojevic <mmedojevicbg@gmail.com>
2020-02-04 23:14:40 +0100

    list implementation

--- ProcMan/LinuxProcMan.php
+++ ProcMan/LinuxProcMan.php
@@ -6,7 +6,22 @@ class LinuxProcMan implements IProcMan
 {
     public function list()
     {
-        return [];
+        $psOutput = shell_exec('ps -A -o pid,command');
+        $psOutput = trim($psOutput);
+        $psLines = explode("\n", $psOutput);
+        $psLines = array_map('trim', $psLines);
+        $psLines = array_map(function ($el) {
+            return explode(' ', $el, 2);
+        }, $psLines);
+        $list = [];
+        for ($i = 1; $i < count($psLines); ++$i) {
+            $list[] = [
+                'pid' => $psLines[$i][0],
+                'title' => $psLines[$i][1],
+            ];
+        }
+
+        return $list;
     }
 
     public function kill($pid)
--- ProcMan/WinProcMan.php
+++ ProcMan/WinProcMan.php
@@ -6,7 +6,27 @@ class WinProcMan implements IProcMan
 {
     public function list()
     {
-        return [];
+        $taskList = shell_exec('tasklist 2>NUL');
+        $taskList = trim($taskList);
+        $taskTable = explode("\n", trim($taskList));
+        $delimiters = explode(' ', $taskTable[1]);
+        $titleColumnStart = 0;
+        $titleColumnLength = strlen($delimiters[0]);
+        $pidColumnStart = strlen($delimiters[0]) + 1;
+        $pidColumnLength = strlen($delimiters[1]);
+        $list = [];
+        foreach ($taskTable as $key => $task) {
+            if ($key > 1) {
+                $pid = (int) substr($task, $pidColumnStart, $pidColumnLength);
+                $title = substr($task, $titleColumnStart, $titleColumnLength);
+                $list[] = [
+                    'pid' => $pid,
+                    'title' => $title,
+                ];
+            }
+        }
+
+        return $list;
     }
 
     public function kill($pid)
@@ -17,6 +37,7 @@ class WinProcMan implements IProcMan
     public function running($pid)
     {
         $taskList = shell_exec('tasklist 2>NUL');
+        $taskList = trim($taskList);
         $taskTable = explode("\n", trim($taskList));
         $delimiters = explode(' ', $taskTable[1]);
         $pidColumnStart = strlen($delimiters[0]) + 1;
